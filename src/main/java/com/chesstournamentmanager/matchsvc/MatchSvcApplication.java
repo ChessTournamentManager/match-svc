0ecaa70089c025cc11a096af7b342c3eede7bdf1
Judah <i460958@fhict.nl>
2022-09-08 00:40:01 +0200

    Added spring boot project

---
+++ src/main/java/com/chesstournamentmanager/matchsvc/MatchSvcApplication.java
@@ -0,0 +1,13 @@
+package com.chesstournamentmanager.matchsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chesstournamentmanager/matchsvc/MatchSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chesstournamentmanager.matchsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
