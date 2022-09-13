4c7efdb17053cb63c43e68e0b2d74777a53d7f69
Judah <i460958@fhict.nl>
2022-09-13 18:38:02 +0200

    Added docker files

--- src/main/java/com/chesstournamentmanager/matchsvc/MatchSvcApplication.java
+++ src/main/java/com/chesstournamentmanager/matchsvc/MatchSvcApplication.java
@@ -2,10 +2,18 @@ package com.chesstournamentmanager.matchsvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MatchSvcApplication {
 
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to the match service.";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MatchSvcApplication.class, args);
 	}
